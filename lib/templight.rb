13d861f3d30b93f6a94d2c6032e3789793b6002c
Stephen Breyer-Menke <steve.bm@gmail.com>
2013-11-07 10:08:22 +0200

    Add template path configure options and bump version

--- lib/templight.rb
+++ lib/templight.rb
@@ -9,9 +9,14 @@ class ErbBinding < OpenStruct
 end
 
 module Templight
+  @template_path = '.'
 
-  def self.render(path, args)
-    template_string = File.read(path)
+  def self.configure(options={:path => @template_path})
+    @template_path = options[:path]
+  end
+
+  def self.render(filename, args)
+    template_string = File.read(File.join(@template_path, filename))
     template = ERB.new template_string
     params = ErbBinding.new(args)
     return template.result(params.get_binding)
