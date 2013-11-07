5fbcf960d608208c9c92a16c85db9cb43412e0bc
Stephen Breyer-Menke <steve.bm@gmail.com>
2013-11-07 13:43:08 +0200

    Fix the config

--- lib/templight.rb
+++ lib/templight.rb
@@ -9,16 +9,16 @@ class ErbBinding < OpenStruct
 end
 
 module Templight
-  @template_path = '.'
+  @template_path = './app/templates'
 
-  def self.configure(options={:path => @template_path})
-    @template_path = options[:path]
+  def self.configure(options={"path" => @template_path})
+    @template_path = options["path"]
   end
 
-  def self.render(filename, args)
+  def self.render(filename, options={})
     template_string = File.read(File.join(@template_path, filename))
     template = ERB.new template_string
-    params = ErbBinding.new(args)
+    params = ErbBinding.new(options)
     return template.result(params.get_binding)
   end
 
